6d0bb3b0eea0841251d3b3fc0fe8178147888b9f
Divyansh Saurabh <158085349+imh-10026-22@users.noreply.github.com>
2024-02-21 00:58:39 +0530

    Update question6.java

--- Assignment_5/question6.java
+++ Assignment_5/question6.java
@@ -19,5 +19,21 @@ class Main4{
         Scanner sc=new Scanner(System.in);
         myIntWrapper m1= new myIntWrapper(10);
         System.out.println("The wrapped integer is :"+m1.getInt());
+        
+        ArrayList<myIntWrapper> myArray = new ArrayList<myIntWrapper>(); 
+        System.out.println("Enter the number of integers to be added to the list:");
+        int n=sc.nextInt();
+        myIntWrapper wrapArray[] = new myIntWrapper[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the "+(i+1)+" element:");
+            int d=sc.nextInt();
+            wrapArray[i]= new myIntWrapper(d);
+            myArray.add(wrapArray[i]);
+        }
+        System.out.println("-----------");
+        
+        for(int i = 0;i<n;i++){
+            System.out.println("The "+(i+1)+" element in the array is : "+(myArray.get(i).getInt()));
+        }
     }
 }
