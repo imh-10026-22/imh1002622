540a7b48ecf7b6763fa627c151c1c2d3f801e72b
Divyansh Saurabh <158085349+imh-10026-22@users.noreply.github.com>
2024-04-15 19:42:08 +0530

    Create question3.java

---
+++ Assignment_6/question3.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+ 
+public class question3 {
+    
+}
+ 
+class Student2{
+    String Student_name;
+    int Student_roll;
+    float Student_fee;
+    
+    Student2(String Student_name,int Student_roll,float Student_fee){
+        this.Student_name=Student_name;
+        this.Student_roll=Student_roll;
+        this.Student_fee=Student_fee;
+    }
+    
+    Student2(){
+        this("Unknown",000,000);   
+    }
+    
+    void Show(){
+        Display(this);
+    }
+    
+    void Display(Object a){
+        System.out.println("The name of the Student is :"+this.Student_name);
+        System.out.println("The roll number of the student is :"+this.Student_roll);
+        System.out.println("The Fee paid by the Student is :"+this.Student_fee);
+    }
+}
+ 
+class Main2{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of Students you want to add details for:");
+        int n=sc.nextInt();
+        String name;
+        int roll;
+        float fee;
+        Student2 S1 = new Student2();
+        Student2 S[]=new Student2[n];
+        System.out.println("-----------");
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the name of the "+(i+1)+" Student:");
+            sc.nextLine();
+            name = sc.nextLine();
+            System.out.println("Enter the  roll number of the Student:");
+            roll = sc.nextInt();
+            System.out.println("Enter the fee paid by the Student: ");
+            fee = sc.nextFloat();
+            S[i] = new Student2(name,roll,fee);
+            System.out.println("-----------");
+        }
+        System.out.println("Result of non parameterized constructor:");
+        S1.Show();
+        System.out.println("-----------");
+        for(int i=0;i<n;i++){
+            System.out.println("Details of Student : " + (i+1));
+            S[i].Show();
+            System.out.println("-----------");
+        }
+    }
+}
